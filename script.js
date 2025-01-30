0f45555a98995de396e595603769bd3d8d69507f
vladcaulfield <vladkryzhenkov8@gmail.com>
2025-01-30 15:39:10 +0300

    feat: add grid generator function

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const divContainer = document.querySelector("#container");
 const divContainerHeight = document.querySelector("#height");
+const divContainerWidth = document.querySelector("#width");
 
 function makeGrid(width, height) {
-  for (let i = 0; i < height; i++) {
+  for (let j = 0; j < height; j++) {
+    let gridRow = document.createElement("div");
+    gridRow.classList.add("row");
     for (let i = 0; i < width; i++) {
       let divGrid = document.createElement("div");
-      divGrid.setAttribute(
-        "style",
-        "width: 16px; height:16px; border: 1px solid black;"
-      );
-      divContainer.appendChild(divGrid);
+      divGrid.classList.add("grid-item");
+      gridRow.appendChild(divGrid);
     }
+    divContainerHeight.appendChild(gridRow);
   }
 }
 
-makeGrid(16, 16);
+makeGrid(24, 24);
