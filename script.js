00c3910c828d524b6b6c6794f2715e8fb2db6399
vladcaulfield <vladkryzhenkov8@gmail.com>
2025-01-31 15:18:00 +0300

    fix: make grid containers response

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ let width = 16;
 let height = 16;
 
 function makeGrid(width, height) {
-  function drawPen(color) {
-    divGrid.style.backgroundColor = `${color}`;
-  }
+  const containerWidth = 450;
+  const squareSize = containerWidth / width;
   for (let j = 0; j < height; j++) {
     let gridRow = document.createElement("div");
     gridRow.classList.add("row");
     for (let i = 0; i < width; i++) {
       let divGrid = document.createElement("div");
       divGrid.classList.add("grid-item");
+      divGrid.style.height = `${squareSize}px`;
+      divGrid.style.width = `${squareSize}px`;
       gridRow.appendChild(divGrid);
       // adding hover effect
       divGrid.addEventListener(
@@ -31,14 +32,16 @@ function makeGrid(width, height) {
 buttonReset.addEventListener("click", gridButton);
 
 function gridButton() {
-  const newSize = prompt("size?", height);
-  if (newSize !== 101) {
-    height = newSize;
-    width = newSize;
-    divContainer.innerHTML = "";
-    makeGrid(width, height);
-    console.log(height, width);
+  const newSize = parseInt(prompt("size?(max 100", height));
+  if (isNaN(newSize) || newSize <= 0 || newSize > 100) {
+    alert("only between 0 - 100.");
+    return;
   }
+  height = newSize;
+  width = newSize;
+  divContainer.innerHTML = "";
+  makeGrid(width, height);
+  console.log(height, width);
 }
 
 makeGrid(26, 26);
